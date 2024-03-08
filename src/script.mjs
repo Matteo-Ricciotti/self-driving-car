7d32cdf7d0dd658220003c1f6a15bf2c63b951d6
Matteo Ricciotti <ricciottimatteo2003@gmail.com>
2024-03-08 21:40:52 +0100

    fix

--- src/script.mjs
+++ src/script.mjs
@@ -5,8 +5,6 @@ const ctx = canvas.getContext('2d');
 
 const car = new Car(100, 100, 30, 50);
 
-car.draw(ctx);
-
 const resetCanvas = () => {
   canvas.height = window.innerHeight;
   canvas.width = 200;
